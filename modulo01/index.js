56f4bedf0566fde972b73dc24aaa96ef13e92269
bsicchieri <brunosicchieri@hotmail.com>
2019-10-18 20:30:54 -0300

    modulo01

--- modulo01/index.js
+++ modulo01/index.js
@@ -3,8 +3,10 @@ const express = require('express');
 const server = express();
 
 //localhost:3000/teste
-server.get('/teste', (req, res) => {
-  return res.json({ message: 'Hello World' });
+server.get('/users/', (req, res) => {
+  const nome = req.query.nome;
+
+  return res.json({ message: `Hello ${nome}` });
 })
 
 server.listen(3000);
