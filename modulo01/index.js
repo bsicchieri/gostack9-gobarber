ae0d1223f8a5e284de276f2cfc215ebdfe972fc4
bsicchieri <brunosicchieri@hotmail.com>
2019-10-18 20:17:52 -0300

    modulo01

--- modulo01/index.js
+++ modulo01/index.js
@@ -1,12 +1,8 @@
-//importando o express - require(nome da dependência)
 const express = require('express');
 
-//criando uma variável(server)
 const server = express();
 
 //localhost:3000/teste
-//req - todos os dados da requisição
-//res - todas as informações necessárias para responder
 server.get('/teste', (req, res) => {
   return res.json({ message: 'Hello World' });
 })
