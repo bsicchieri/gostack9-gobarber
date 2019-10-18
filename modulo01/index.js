b47552cea0652048f8a6ea29831211e0d4bb9df5
bsicchieri <brunosicchieri@hotmail.com>
2019-10-18 20:35:53 -0300

    modulo01

--- modulo01/index.js
+++ modulo01/index.js
@@ -3,10 +3,14 @@ const express = require('express');
 const server = express();
 
 //localhost:3000/teste
-server.get('/users/', (req, res) => {
-  const nome = req.query.nome;
+server.get('/users/:id', (req, res) => {
+  //const id = req.params.id;
+  const { id } = req.params;
 
-  return res.json({ message: `Hello ${nome}` });
+  return res.json({ message: `Buscando o usuário ${id}` });
+
+  //const nome = req.query.nome;
+  //return res.json({ message: `Hello ${nome}` });
 })
 
 server.listen(3000);
