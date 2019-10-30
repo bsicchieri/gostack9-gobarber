99575d4531ff24fc292134e20659b819a5d5437d
bsicchieri <brunosicchieri@hotmail.com>
2019-10-30 17:18:28 -0300

    Starting GoBarber Backend

--- modulo02/src/app/middlewares/auth.js
+++ modulo02/src/app/middlewares/auth.js
@@ -1,3 +1,5 @@
+// verificar se o usuário está logado
+
 import jwt from 'jsonwebtoken';
 import { promisify } from 'util';
 
@@ -6,19 +8,23 @@ import authConfig from '../../config/auth';
 export default async (req, res, next) => {
   const authHeader = req.headers.authorization;
 
+  // se não estiver presente na requisição
   if (!authHeader) {
     return res.status(401).json({ error: 'Token not provided ' });
   }
 
+  // dividir o bearer e o token
   const [, token] = authHeader.split(' ');
 
   try {
+    // se conseguiu decifrar o token, decoded recebe as informações
     const decoded = await promisify(jwt.verify)(token, authConfig.secret);
 
     req.userId = decoded.id;
 
     return next();
   } catch (err) {
+    // caso o token esteja errado
     return res.status(401).json({ error: 'Token invalid' });
   }
 };
--- modulo02/src/routes.js
+++ modulo02/src/routes.js
@@ -10,6 +10,7 @@ const routes = new Router();
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
 
+// só vale para as rotas que vierem após
 routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
