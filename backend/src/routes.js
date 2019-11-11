cc6047d2f6f0e8e30377b1058a881ece80496494
bsicchieri <brunosicchieri@hotmail.com>
2019-11-11 12:51:43 -0300

    backend

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -19,6 +19,7 @@ routes.use(authMiddleware);
 routes.put('/users', UserController.update);
 
 routes.post('/files', upload.single('file'), (req, res) => {
+  // return res.json(req.file);
   return res.json({ ok: true });
 });
 
