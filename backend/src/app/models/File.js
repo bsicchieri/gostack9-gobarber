0130df8bc7155ac25aea51adaf592299ada2cf53
bsicchieri <brunosicchieri@hotmail.com>
2019-11-11 13:06:45 -0300

    backend

--- backend/src/app/models/File.js
+++ backend/src/app/models/File.js
@@ -5,7 +5,7 @@ class File extends Model {
     super.init(
       {
         name: Sequelize.STRING,
-        pathl: Sequelize.STRING,
+        path: Sequelize.STRING,
       },
       {
         sequelize,
