c29c2b6037a6e0b3d3078e1c2812a3edbd92f18e
bsicchieri <brunosicchieri@hotmail.com>
2019-11-14 13:45:54 -0300

    backend

--- backend/src/app/controllers/AppointmentController.js
+++ backend/src/app/controllers/AppointmentController.js
@@ -111,6 +111,10 @@ class AppointmentController {
 
     return res.json(appointment);
   }
+
+  async delete(req, res) {
+    return res.json();
+  }
 }
 
 export default new AppointmentController();
